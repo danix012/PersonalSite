7e9a3e92b53aaf220835bae204c20efd65c4b0da
danix012 <danica.prz@gmail.com>
2020-10-26 10:25:10 +1300

    drop down menu edit

--- PERSONALSITE.js
+++ PERSONALSITE.js
@@ -1,39 +1,31 @@
-
-    function openNav() {
-      document.getElementById("mySidenav").style.width = "250px";
-      document.getElementById("main").style.marginLeft = "250px";
-      document.body.style.backgroundColor = "#d2f5e3";
-      document.getElementById("main").onclick = function closeNav() {
-      document.getElementById("mySidenav").style.width = "0";
-      document.getElementById("main").style.marginLeft= "0";
-      document.body.style.backgroundColor = "#d2f5e3";
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "#d2f5e3";
+    document.getElementById("main").onclick = function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+        document.body.style.backgroundColor = "#d2f5e3";
     }
-    }
-    
-   
+}
 
-    function closeNav() {
-      document.getElementById("mySidenav").style.width = "0";
-      document.getElementById("main").style.marginLeft= "0";
-      document.body.style.backgroundColor = "#d2f5e3";
-    }
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "#d2f5e3";
+}
 
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
 
-
-    // function openDropdown() {
-    //   document.getElementById("dropdown-container").style.height = "0";
-   
-    //   var dropdown = document.getElementsByClassName("dropdown-btn");
-    // var i;
-    
-
-// if        dropdown[i].addEventListener("mouseover", function() {
-//       this.classList.toggle("active");
-//       var dropdownContent = this.nextElementSibling;
-//       if (dropdownContent.style.display === "block") {
-//       dropdownContent.style.display = "none";
-//       } else {
-//       dropdownContent.style.display = "block";
-//       }
-//       });
-//     }
+for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("mouseover", function() {
+        this.classList.toggle("active");
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+        } else {
+            dropdownContent.style.display = "block";
+        }
+    });
+}
